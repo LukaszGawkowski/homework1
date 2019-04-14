986652ea61af06fbda1b9fbc09cd433a69413fb8
LukaszGawkowski <lukasz.gawkowski@gmail.com>
2019-04-14 14:05:34 +0200

    1) Method added: Create random numbers list. Parameter of the method = how many numbers will be drawn. 2) Method added: Find min/max number from drawn, random number list, with java collections. 3)Method added: Same goal as 2) method, but with no collections usage.
    TODO: Make all 3 methods use same drawn, random number list.

--- src/main/java/com/homeworks/homework1/InputControler.java
+++ src/main/java/com/homeworks/homework1/InputControler.java
@@ -1,4 +1,33 @@
 package com.homeworks.homework1;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+
+@Controller
 public class InputControler {
+
+    @Autowired
+    SerwisRandom serwis;
+
+//         Metody zawarte w Controllerze nie powinny być typu void, ponieważ trzeba coś zwrócić klientowi który do niego strzela.
+
+        @GetMapping (value = "random")
+        public void getDrawnNumbersListMaxMin (@RequestParam(value = "number", defaultValue = "0") int number, @RequestParam(value = "flag") String flag) {
+            List<Integer> list = serwis.getDrawnNumbersList(number);
+            System.err.println("Drawn numbers are: " + list);
+
+            Integer maxMinCollections = serwis.getMinOrMaxUsageOfCollections(serwis.getDrawnNumbersList(number),flag);
+            System.err.println();
+            System.err.println(flag + " number is: " + maxMinCollections);
+
+            Integer maxMinNoCollections = serwis.getMinOrMaxNoCollectionsUsage(serwis.getDrawnNumbersList(number),flag);
+            System.err.println(flag + " number is: " + maxMinNoCollections);
+        }
+
+
 }
--- src/main/java/com/homeworks/homework1/SerwisRandom.java
+++ src/main/java/com/homeworks/homework1/SerwisRandom.java
@@ -1,26 +1,72 @@
 package com.homeworks.homework1;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
-public class ServisRandom {
-
-    public void drawingRandomNumbers() {
-
+@Service
+public class SerwisRandom {
 
         // czy mozona uzywac w nazwach metod slow get/set? Czy nie wprowadzaja w blad czytelnika (ref. settery i gettery zmiennych)
 
-        public void getDrawnNumbersList(int x){
+        public List<Integer> getDrawnNumbersList(int x) {
 
-            List<Integer> list = new ArrayList<Integer>();
+            if (x > 0) {
+                int drawnNumber;
+                Random r = new Random();
+                List<Integer> drawnNumbersList = new ArrayList<Integer>();
 
+                for (int n = 1; n <= x; n++) {
+                    drawnNumber = r.nextInt(1000);
+                    drawnNumbersList.add(drawnNumber);
+                }
 
+                return drawnNumbersList;
+
+            } else throw new IllegalArgumentException("Provided number is not positive!");
+        }
+        
+        public Integer getMinOrMaxUsageOfCollections (List<Integer> list, String flag){
+
+            List<Integer> drawnList = list;
+
+            Integer x;
+            if("max".equals(flag)){
+                x = Collections.max(drawnList);
+            } else if ("min".equals(flag)){
+                x = Collections.min(drawnList);
+            } else throw new IllegalArgumentException("Provided flag must be: max/min!");
+
+            return x;
+        }
+
+        public Integer getMinOrMaxNoCollectionsUsage (List<Integer> list, String flag){
+
+            List<Integer> drawnList = list;
+
+            Integer y = drawnList.get(0);
+
+            if("max".equals(flag)){
+                for(int n = 0; n <= drawnList.size(); n++) {
+                    if (y <= drawnList.get(n)) {
+                        y = drawnList.get(n);
+                    }
+                }
+            } else if ("min".equals(flag)){
+
+                for(int n = 0; n <= drawnList.size(); n++) {
+                    if (y >= drawnList.get(n)) {
+                        y = drawnList.get(n);
+                    }
+                }
+
+            }else throw new IllegalArgumentException("Provided flag must be: max/min!");
+
+            return y;
         }
 
 
-
-    }
-    //tutaj ma być losowanie, random
-
-    // tutaj mam if'a sprawdzajacego czy wartosc jest x>0
 }
